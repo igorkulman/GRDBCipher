071b62fafeafeac8e132fcfa23471ab3fc513cf1
Igor Kulman <igor@kulman.sk>
2019-05-03 09:14:47 +0200

    🎉 working build with an aggregated target

---
+++ GRDBCipher/GRDBCipher.h
@@ -0,0 +1,19 @@
+//
+//  GRDBCipher.h
+//  GRDBCipher
+//
+//  Created by Igor Kulman on 02/05/2019.
+//  Copyright © 2019 Igor Kulman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRDBCipher.
+FOUNDATION_EXPORT double GRDBCipherVersionNumber;
+
+//! Project version string for GRDBCipher.
+FOUNDATION_EXPORT const unsigned char GRDBCipherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRDBCipher/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'GRDBCipher' do
+    # GRDB with SQLCipher 3
+    pod 'GRDB.swift/SQLCipher', :git => 'https://github.com/groue/GRDB.swift', :branch => 'GRDB-4.0'
+    pod 'SQLCipher', '3.4.2'
+end  
